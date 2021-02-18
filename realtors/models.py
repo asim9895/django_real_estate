eb094a343c9e352717cc57b6c489f27bbd98b77f
asim <asimjaipuri99@gmail.com>
2021-02-18 11:05:39 +0530

    added listing and realtors data through admin panel

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Realtors(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    is_mvp = models.BooleanField(default=True)
+    hire_date = models.DateTimeField(default=datetime.now , blank=True)
+
+    def __str__(self):
+        return self.name
+    
