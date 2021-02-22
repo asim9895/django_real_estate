16af011e30743aaecbd06444a737c2721e816d95
asim <asimjaipuri99@gmail.com>
2021-02-22 14:26:57 +0530

    few issues to fix

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing_id']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        user_id = request.POST['user_id']
+        message = request.POST['message']
+
+        contact = Contact(listing=listing , listing_id=listing_id,  name=name, email=email , 
+           message=message , user_id=user_id , phone=phone)
+        
+        contact.save()
+
+        messages.success(request , 'Your request has been submitted , a realtor will get back to you soon')
+        return redirect('/listings/'+listing_id)
